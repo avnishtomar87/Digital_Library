e3e454d68eca5b89cecbcf21cdd60f5ce4c775b5
avnishtomar87 <99217520+avnishtomar87@users.noreply.github.com>
2022-04-04 01:39:30 +0530

    Add files via upload

---
+++ Backend/routes/pool.js
@@ -0,0 +1,12 @@
+var mysql=require('mysql')
+var pool=mysql.createPool({
+    host:'localhost',
+    port:3306,
+    user:'root',
+    password:'123',
+    database:'vs',
+    connectionLimit:100,
+    multipleStatements:true
+    })
+    
+module.exports = pool;
